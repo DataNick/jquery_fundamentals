7669d852031ab3298106ad76b0781edd53cfeba1
DataNick <DataNick@users.noreply.github.com>
2014-11-21 14:51:29 -0500

    Initial commit with two exercises complete

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -5,10 +5,20 @@
 // Change the background color of '#target' by script. 
 // Hint: you can use .css
 
+
+// $(document).ready(function() {
+//   $('#target').css('background-color', 'red');
+// });
+
+
 // EXERCISE: Change Parent
 
 // Change the text in the span, a child of "#target"
 // Hint: you can use .text
+
+$(document).ready(function (){
+	$('span').text('I am a butterfly');
+});
 
 // EXERCISE: Create Clone
 
