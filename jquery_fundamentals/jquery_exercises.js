ee34c03cd97d58edb6024714a5982ac74084f106
DataNick <DataNick@users.noreply.github.com>
2014-11-22 17:25:31 -0500

    finished exercises

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -53,9 +53,9 @@
 // Uncheck all checkboxes using jQuery
 // Hint: you will have to select the input and can use .removeAttr
 
-$(document).ready(function(){
-  $('input').removeAttr('checked');
-});
+// $(document).ready(function(){
+//   $('input').removeAttr('checked');
+// });
 
 
 // EXERCISE: Change Parent
@@ -63,49 +63,114 @@ $(document).ready(function(){
 // Move "#child" from "#parent1" to "#parent2"
 // Hint: you can use .appendTo
 
+// $(document).ready(function() {
+// 	$('#child').appendTo('#parent2');
+// });
+
+
+
 // EXERCISE: Select A Option in A Select Box
 
 // Select the second option in the selectbox by script
 // Hint: you will have to select the option and can use .eq and .attr
+
+// $(document).ready(function(){
+// 	$('option').eq(1).attr('selected', 'selected');
+// });
+
 
 // EXERCISE: Change Size
 
 // Make "#target" double size
 // Hint: you can use .css, .width and .height
 
+// $(document).ready(function (){
+// 	$('#target').css('width', '300px').css('height', '200px');
+      
+// });
+
+
+
 // EXERCISE: Empty Elements
 // Hint: you can use .empty
 
 // Remove all children and text of "#target"
 
+// $(document).ready(function(){
+// 	$('#target').empty('child');
+// });
+
 // EXERCISE: Delay
 
 // Show Alert with 1 second delay (Use "setTimeout")
 // Hint: you can use .setTimeout and alert
+// $(document).ready(function(){
+// 	$(this).setTimeout(function(){
+//       alert 'Time out';
+// 	});
+// });
+
+// $(document).ready(function () {
+//     setTimeout("slowAlert('Meow')",5000);
+// });
 
 // EXERCISE: Count
 
 // Show the number of children in an alert
 // Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
 
+// $(document).ready(function(){
+//   ('#target').find('.child').count('size')
+// });
+
+// $(document).ready(function () {
+//    alert($("#target .child").length);
+// });
+
 // EXERCISE: Animate
 
 // Make "#target" double size with animation
 // Hint: you can use .animate, .width, and .height
+
+// $(document).ready(function(){
+// 	$('#target').animate({
+// 		height: "+=200",
+// 		width: "+=200",
+// 	},
+// 		'elastic'
+// 	);
+// });
+
+// $(document).ready(function () {
+//    $("#target").animate({height: "+=190", width: "+=150"},'slow');
+// });
 
 // EXERCISE: Alternate Color
 
 // Make the list-box alternating. (Make odd options a different color)
 // Hint: you can select the option and use .filter and .css
 
+// $(document).ready(function(){
+// 	$('option').filter(':odd').css('background-color', 'yellow');
+// });
+
 // EXERCISE: All But One
 
 // Remove all children of "#target" but h2
 // Hint: you can use .children, .not and .remove
 
+// $(document).ready(function(){
+// 	$('#target').children().not('h2').remove();
+// });
+
 // EXERCISE: Without Children
 
 // Remove all 'div' whitch has no child elements.
 // Hint: you can use .not and .remove. Look closely at what options you can pass .not
+
+$(document).ready(function(){
+	$('div').remove(':empty');
+	
+});
 
 
